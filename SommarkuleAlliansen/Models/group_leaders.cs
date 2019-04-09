5853fd47b75156b9fc1fc0af297761de39ceec23
adamblomkvist <AdamBlomkvist@hotmail.com>
2019-04-09 11:50:48 +0200

    klasser

---
+++ SommarkuleAlliansen/Models/group_leaders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SommarkuleAlliansen.Models
+{
+    public class group_leaders
+    {
+        public int group_leaders_id { get; set; }
+        public int group_number { get; set; }
+        public int employe_id { get; set; }
+    }
+}
---
+++ SommarkuleAlliansen/Models/location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SommarkuleAlliansen.Models
+{
+    public class location
+    {
+        public int location_id { get; set; }
+        public string name { get; set; }
+        public string adress { get; set; }
+        public DateTime date { get; set; }
+    }
+}
