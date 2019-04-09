05b502894c727fa42f97658e82a8a6df7297c627
Jesper Granbo <jesper_granbo@hotmail.com>
2019-04-09 11:49:38 +0200

    klasser

---
+++ SommarkuleAlliansen/Models/groups.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SommarkuleAlliansen.Models
+{
+    public class groups
+    {
+        public int group_id { get; set; }
+        public int child_id { get; set; }
+        public int group_number { get; set; }
+        public bool present { get; set; }
+    }
+}
