f70bb6e288f97d82921ff0fc376c666496d9538d
adamblomkvist <AdamBlomkvist@hotmail.com>
2019-05-14 15:31:35 +0200

    employe models

--- SommarkuleAlliansen/Models/employe.cs
+++ SommarkuleAlliansen/Models/employe.cs
@@ -14,5 +14,16 @@ namespace SommarkuleAlliansen.Models
         public string password { get; set; }
         public int group_id { get; set; }
         public int location_id { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public int post_number { get; set; }
+        public Boolean tax { get; set; }
+        public string bank { get; set; }
+        public int clearing { get; set; }
+        public int account_number { get; set; }
+        public string shirt_size { get; set; }
+        public int social_security { get; set; }
+
+
     }
 }
